f652890042140551170b9a634b286af487c5fcce
nihal <nihal.mitta@gmail.com>
2023-07-28 21:54:54 -0500

    Can now select correct chords

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from services.services import get_chords
 from midiCalculations import *
 
 app = Flask(__name__)
@@ -10,6 +11,16 @@ def progression_notes():
     response = jsonify(notes)
 
     return response
+
+@app.route('/chords', methods=['GET'])
+def chords():
+    # Get the chord type from the query parameters
+    chord_type = request.args.get('type', default='major', type=str)
+
+    # Call get_chords() with the specified chord type
+    chords = get_chords(chord_type)
+
+    return jsonify(chords)
 
 if __name__ == '__main__':
   app.run(debug=True)
---
+++ flask-server/services/services.py
@@ -0,0 +1,5 @@
+def get_chords(type: str) :
+    if type == "major":
+        return ["I", "ii", "iii", "IV", "V", "vi", "vii"]
+    else:
+        return ["i", "ii°", "III", "iv", "v", "VI", "VII"]
