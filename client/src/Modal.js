87f9b66c58e0d179a6a9860c452dbaec0f49d494
nihal <nihal.mitta@gmail.com>
2023-07-30 18:40:05 -0500

    Chords now as an overlay

--- client/src/Modal.js
+++ client/src/Modal.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './Modal.css';
 
 const Modal = ({ isOpen, onClose, onSelect, chords }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="modal">
-      <button onClick={onClose}>Close</button>
-      <ul>
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button onClick={onClose}>Close</button>
         {chords.map((chord, index) => (
-          <li key={index} onClick={() => onSelect(chord)}>{chord}</li>
+          <button key={index} onClick={() => onSelect(chord)}>{chord}</button>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
